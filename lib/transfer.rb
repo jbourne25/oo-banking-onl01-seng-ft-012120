7df5dcacaacb86a2d7053e1107c7feeabb253735
Jeremy Bourne <jbourne25@yahoo.com>
2020-02-06 06:39:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,17 +19,15 @@ class Transfer
   end 
   
   def execute_transaction
-    no_funds = "Transaction rejected. Please check your account balance."
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     else
       @status = "rejected"
-      #return "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
     end
     
-    no_funds 
   end 
   
   def reverse_transfer
