77e1ed0407b605ca2cd8ea206cf3126e4f172495
Jeremy Bourne <jbourne25@yahoo.com>
2020-02-06 06:30:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,19 @@ class Transfer
   end 
   
   def execute_transaction 
-   
+    def execute_transaction
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
+    end
+  end
   
   def reverse_transfer
     if @status == "complete"
